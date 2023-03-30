4a10ff5b49c4ebe68bb0ed843665b03dd916b45c
Kurtsev Dmitriy <boman@bk.ru>
2023-03-30 15:07:48 +0500

    add test for junit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,39 +1,39 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void testRemainBoundaryValuesTwo() {
-        assertEquals(service.remain(0), 1000);
+    public void testRemainBoundary1000() {
+        assertEquals(1000,service.remain(0));
     }
 
     @Test
-    public void testRemainBoundaryValuesThree() {
-        assertEquals(service.remain(1), 999);
+    public void testRemainBoundary999() {
+        assertEquals(999,service.remain(1));
     }
 
     @Test
-    public void testRemainBoundaryValuesFour() {
-        assertEquals(service.remain(999), 1);
+    public void testRemainBoundary1() {
+        assertEquals(1, service.remain(999));
     }
 
     @Test
-    public void testRemainBoundaryValuesFive() {
-        assertEquals(service.remain(1000), 0);
+    public void testRemainBoundary0() {
+        assertEquals(0,service.remain(1000));
     }
 
     @Test
-    public void testRemainBoundaryValuesSix() {
-        assertEquals(service.remain(1001), 999);
+    public void testRemain999() {
+        assertEquals(999,service.remain(1001));
     }
 
     @Test
-    public void testRemainCriticalPath() {
-        assertEquals(service.remain(200), 800);
+    public void testRemain800() {
+        assertEquals(800, service.remain(200));
     }
 }
