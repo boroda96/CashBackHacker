741bcf6b1f1ed413252a28a9e2351d05258cc5c3
Kurtsev Dmitriy <boman@bk.ru>
2023-03-30 14:48:13 +0500

    add Tests in branch testng

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,39 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void testRemainBoundaryValuesTwo() {
+        assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void testRemainBoundaryValuesThree() {
+        assertEquals(service.remain(1), 999);
+    }
+
+    @Test
+    public void testRemainBoundaryValuesFour() {
+        assertEquals(service.remain(999), 1);
+    }
+
+    @Test
+    public void testRemainBoundaryValuesFive() {
+        assertEquals(service.remain(1000), 0);
+    }
+
+    @Test
+    public void testRemainBoundaryValuesSix() {
+        assertEquals(service.remain(1001), 999);
+    }
+
+    @Test
+    public void testRemainCriticalPath() {
+        assertEquals(service.remain(200), 800);
+    }
+}
