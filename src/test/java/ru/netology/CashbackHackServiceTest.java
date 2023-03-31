02550d6687200f4f7c8e36702345834d2637461a
Kurtsev Dmitriy <boman@bk.ru>
2023-03-31 10:31:46 +0500

    add test fail

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,39 +1,39 @@
 package ru.netology;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
     public void testRemainBoundaryValuesTwo() {
-        assertEquals(service.remain(0), 1000);
+        Assert.assertEquals(service.remain(0), 1000);
     }
 
     @Test
     public void testRemainBoundaryValuesThree() {
-        assertEquals(service.remain(1), 999);
+        Assert.assertEquals(service.remain(1), 999);
     }
 
     @Test
     public void testRemainBoundaryValuesFour() {
-        assertEquals(service.remain(999), 1);
+        Assert.assertEquals(service.remain(999), 1);
     }
 
-//    @Test
-//    public void testRemainBoundaryValuesFive() {
-//        assertEquals(service.remain(1000), 0);
-//    }
+    @Test
+    public void testRemainBoundaryValuesFive() {
+        Assert.assertEquals(service.remain(1000), 0);
+    }
 
     @Test
     public void testRemainBoundaryValuesSix() {
-        assertEquals(service.remain(1001), 999);
+        Assert.assertEquals(service.remain(1001), 999);
     }
 
     @Test
     public void testRemainCriticalPath() {
-        assertEquals(service.remain(200), 800);
+        Assert.assertEquals(service.remain(200), 800);
     }
 }
